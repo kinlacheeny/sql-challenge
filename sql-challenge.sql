0e1a26f84813ec9dcd583b2df3b95923b1d8eddc
JB Kinlacheeny <71355546+kinlacheeny@users.noreply.github.com>
2020-12-29 19:10:29 -0500

    push updated sql-cchallenge.sql

--- sql-challenge.sql
+++ sql-challenge.sql
@@ -1,19 +1,69 @@
-CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR NOT NULL ,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR(50) NOT NULL,
-	last_name VARCHAR(50) NOT NULL,
-	sex VARCHAR(1) NOT NULL, 
-	hire_date DATE NOT NULL, 
+-- Create tables from csv. 
+--DROP TABLE departments;
+CREATE TABLE departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	PRIMARY KEY (dept_no)
 );
 
-employees
---
-emp_no INT pk
-emp_title_id FK >- titles.title_id
-birth_date date
-first_name varchar(50)
-last_name varchar(50)
-sex Boolean
-hire_date date
+SELECT * FROM departments;
+--*************************************
+-- Create TABLE for dept_emp
+--DROP TABLE dept_emp
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_emp;
+--*************************************
+-- Create table for dept_manager
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL, 
+	emp_no INT NOT NULL, 
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM dept_manager;
+--*************************************
+-- Create table for employees
+--DROP TABLE employees;
+CREATE TABLE employees (
+	emp_no INT NOT NULL, 
+	emp_title_id VARCHAR NOT NULL, 
+	birth_date DATE NOT NULL, 
+	first_name VARCHAR NOT NULL, 
+	last_name VARCHAR NOT NULL, 
+	sex VARCHAR NOT NULL, 
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id) 
+);
+
+SELECT * FROM employees;
+--*********************************
+-- Create table for salaries
+CREATE TABLE salaries (
+	emp_no INT NOT NULL, 
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries;
+--*************************************
+-- Create table for titles
+--DROP TABLE titles;
+CREATE TABLE titles (
+	title_id VARCHAR NOT NULL, 
+	title VARCHAR NOT NULL,
+	PRIMARY KEY (title_id)
+);
+
+SELECT * FROM titles;
+
+
+
+
